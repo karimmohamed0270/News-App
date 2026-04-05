a20291a1ad71367430e2bc024d285c5522c24968
Karim Mohamed <karimmohamed0270@gmail.com>
2026-04-05 13:47:37 +0200

    first commit

--- lib/widgets/custom_btn.dart
+++ lib/widgets/custom_btn.dart
@@ -11,18 +11,21 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 250,
-      margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blue,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))),
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 250,
+        margin: const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8))),
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
